8e529517cd9c43646bce4f7ab7d531b3be88a994
ian-j-bush <qoalagames@gmail.com>
2026-06-12 09:20:05 -0500

    Initial commit: created Spring Boot boilerplate

---
+++ src/test/java/com/ianjbush/x12parser/X12ParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ianjbush.x12parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X12ParserApplicationTests {
+
+    @Test
+    void contextLoads( ) {
+    }
+
+}
