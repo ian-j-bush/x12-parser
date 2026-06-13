6ba42c4bbf66337cb0a230f3f323edb434696e2f
ian-j-bush <qoalagames@gmail.com>
2026-06-12 19:46:15 -0500

    Adjusted controllers and js to establish controller connection

--- src/main/java/com/ianjbush/x12parser/controller/ClaimController.java
+++ src/main/java/com/ianjbush/x12parser/controller/ClaimController.java
@@ -2,15 +2,14 @@ package com.ianjbush.x12parser.controller;
 
 import com.ianjbush.x12parser.service.ClaimService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/claim")
+@RequestMapping( "/api/claim" )
 public class ClaimController {
-    
+
     private final ClaimService service;
 
     public ClaimController( ClaimService service ) {
@@ -18,7 +17,8 @@ public class ClaimController {
     }
 
     @GetMapping( value = "/{id}", produces = MediaType.TEXT_PLAIN_VALUE )
-    public String post( @RequestBody String value ) {
-        return "index";
+    public Object retrieveClaimData( @PathVariable String id ) {
+
+        return "ID: " + id;
     }
 }
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,5 +1,11 @@
+var id = "";
+const output = document.querySelector('.output');
+
 document.querySelectorAll('.edi-type').forEach((button) => {
-    button.addEventListener('click', ()=> {
+    button.addEventListener('click', async ()=> {
+        id = button.getAttribute('id');
+        const resp = await fetch(`/api/claim/${id}`);
+        console.log(await resp.text());
         enableViews();
     });
 });
@@ -8,4 +14,19 @@ function enableViews() {
     document.querySelectorAll('.output-type').forEach(button => {
         button.disabled = false;
     } )
+}
+
+document.querySelectorAll('.output-type').forEach(button => {
+    button.addEventListener('click', async () => {
+        const mode = button.getAttribute("id");
+        const resp = await fetch(`api/render/${id}/${mode}`, {
+            method: "POST"
+        });
+        const text = await resp.text();
+        render(text);
+    })
+});
+
+function render(text) {
+    output.textContent = text;
 }
